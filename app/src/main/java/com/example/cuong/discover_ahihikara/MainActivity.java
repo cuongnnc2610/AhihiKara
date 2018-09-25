134c83a9ed63e82f5a4a8ddfc139670908acc5df
Cuong Nguyen <cuongnnc2610@gmail.com>
2018-09-25 17:24:28 +0700

    discover+setting

---
+++ app/src/main/java/com/example/cuong/discover_ahihikara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cuong.discover_ahihikara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discover);
+    }
+
+}
---
+++ app/src/main/java/com/example/cuong/discover_ahihikara/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.cuong.discover_ahihikara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
